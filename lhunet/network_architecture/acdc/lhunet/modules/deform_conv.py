21397214db7c876389a4551ce7bcf0c75f6aa052
garv sachdev <83745829+gavkujo@users.noreply.github.com>
2025-01-22 10:43:38 +0800

    Update deform_conv.py

--- lhunet/network_architecture/acdc/lhunet/modules/deform_conv.py
+++ lhunet/network_architecture/acdc/lhunet/modules/deform_conv.py
@@ -15,7 +15,7 @@ from torch.autograd.function import once_differentiable
 
 import sys
 sys.path.append('/opt/conda/lib/python3.10/site-packages')
-import d3d
+import d3d as D3D
 
 class DeformConvFunction(Function):
     @staticmethod
